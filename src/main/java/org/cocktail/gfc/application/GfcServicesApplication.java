9ae57551030328f5f8d003111fb64b4e0badd22b
flagouey <francois.lagoueyte@asso-cocktail.fr>
2017-07-15 13:41:05 +0200

    move application to gfc-Servcies

---
+++ src/main/java/org/cocktail/gfc/application/GfcServicesApplication.java
@@ -0,0 +1,12 @@
+package org.cocktail.gfc.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfcServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GfcServicesApplication.class, args);
+    }
+}
