1f0d608bd706f5f837eaf544b487defeabfdb3b4
François <francois.lagoueyte@asso-cocktail.fr>
2017-10-02 14:10:35 +0200

    [TECH] remove feign

--- depense/depense-core/depense-core-api/src/main/java/org/cocktail/gfc/depense/api/ServiceDescriptor.java
+++ depense/depense-core/depense-core-api/src/main/java/org/cocktail/gfc/depense/api/ServiceDescriptor.java
@@ -1,10 +1,6 @@
 package org.cocktail.gfc.depense.api;
 
-import feign.Param;
-import feign.RequestLine;
-
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
@@ -13,22 +9,19 @@ public interface ServiceDescriptor {
     interface CodeAnalytiqueServiceDescriptor {
         @GET
         @Path("/")
-        @RequestLine("GET /api/v1/gfc/codes-analytique?code={code}&libelle={libelle}")
         List<CodeAnalytiqueRepresentation> find(
-                @QueryParam("code") @Param("code") String code,
-                @QueryParam("libelle") @Param("libelle") String libelle);
+                @QueryParam("code") String code,
+                @QueryParam("libelle") String libelle);
 
         @GET
         @Path("/{code}")
-        @RequestLine("GET /api/v1/gfc/codes-analytique/{code}")
-        CodeAnalytiqueRepresentation getCodeAnalytique(@PathParam("code") @Param("code") String code);
+        CodeAnalytiqueRepresentation getCodeAnalytique(@PathParam("code") String code);
     }
 
     interface DemandePaiementServiceDescriptor {
         @GET
         @Path("/{id}/valider")
-        @RequestLine("GET /api/v1/gfc/demandes-paiement/{id}/valider")
-        Response valider(@PathParam("id") @Param("id") Long idDp);
+        Response valider(@PathParam("id") Long idDp);
 
     }
 }
