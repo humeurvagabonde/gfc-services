f188371fe29eacdba7bf8235fb01d5e29d1d4df7
François <francois.lagoueyte@asso-cocktail.fr>
2017-10-02 14:21:28 +0200

    [TECH] Add SpringBootServletInit

--- app-spring/src/main/java/org/cocktail/gfc/GfcServicesApplication.kt
+++ app-spring/src/main/java/org/cocktail/gfc/GfcServicesApplication.kt
@@ -2,9 +2,10 @@ package org.cocktail.gfc
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class GfcServicesApplication
+class GfcServicesApplication : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     SpringApplication.run(GfcServicesApplication::class.java, *args)
