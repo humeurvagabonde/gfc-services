a59da5126314e6e98cc8e27b5a5e3730c1877cad
Francois Lagoueyte <francois.lagoueyte@asso-cocktail.fr>
2017-12-13 14:21:43 +0100

    [TECH] retour aux bases gradle :)

--- application/src/main/java/org/cocktail/gfc/GfcServicesApplication.java
+++ application/src/main/java/org/cocktail/gfc/GfcServicesApplication.java
@@ -1,13 +1,13 @@
-package org.cocktail.gfc
+package org.cocktail.gfc;
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GfcServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GfcServicesApplication.class, args);
 	}
 
 }
